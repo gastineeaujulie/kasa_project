ff058a89e662bb5325c37fccf31caf994f007c9b
gastineeaujulie <gastineeaujulie@gmail.com>
2026-01-22 15:20:12 +0100

    ouverture et fermeture des collapses

---
+++ src/components/KasaValueItemcopy.jsx
@@ -0,0 +1,20 @@
+import vector from '../assets/vector.svg';
+import { useState } from 'react';
+
+export default function KasaValueItem({ valeur }) {
+  const [isOpen, setItOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setItOpen(!isOpen);
+  };
+
+  return (
+    <li key={valeur.id} className="about-collapse-item">
+      <button onClick={toggleCollapse} className="about-btn">
+        {valeur.id}
+        <img src={vector} alt="vector-icone" className="about-icone" />
+      </button>
+      {isOpen && <div className="about-description">{valeur.description}</div>}
+    </li>
+  );
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
 import about from '../assets/about.png';
 import '../styles/About.scss';
 import { valeurs } from '../data/ValeursList.js';
-import vector from '../assets/vector.svg';
+import KasaValueItemcopy from '../components/KasaValueItemcopy.jsx';
 
 export default function About() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleCollapse = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
       <div className="about-banner">
@@ -24,18 +17,7 @@ export default function About() {
       <div className="about-container">
         <ul className="about-collapse-list">
           {valeurs.map((valeur) => (
-            <li key={valeur.id} className="about-collapse-item">
-              <button
-                onClick={() => toggleCollapse(valeur.id)}
-                className="about-btn"
-              >
-                {valeur.id}
-                <img src={vector} alt="vector-icone" className="about-icone" />
-              </button>
-              {isOpen && (
-                <div className="about-description">{valeur.description}</div>
-              )}
-            </li>
+            <KasaValueItemcopy key={valeur.id} valeur={valeur} />
           ))}
         </ul>
       </div>
